64a2792f658cf88fe2e0c3f7ef5a02344a3316f0
Leonardo Goandete <leonardo.goandete@hotmail.com>
2023-11-02 17:48:33 -0300

    Add test model Postagem

--- src/test/java/br/com/doasanguepoa/postagem/model/PostagemTest.java
+++ src/test/java/br/com/doasanguepoa/postagem/model/PostagemTest.java
@@ -1,12 +1,16 @@
 package br.com.doasanguepoa.postagem.model;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PostagemTest {
+ class PostagemTest {
 
     @Test
-    public void testConstrutorEPropriedades() {
+     void testConstrutorEPropriedades() {
         String mensagem = "Teste de mensagem";
         Postagem postagem = new Postagem(mensagem);
         postagem.setId(1L);
@@ -15,5 +19,46 @@ public class PostagemTest {
         assertEquals(mensagem, postagem.getMensagem());
         assertNotNull(postagem.getCreatedAt());
         assertNotNull(postagem.getUpdateAt());
+    }
+
+    @Test
+     void testAtualizacaoDaMensagem() {
+        String mensagemOriginal = "Mensagem original";
+        Postagem postagem = new Postagem(mensagemOriginal);
+
+        String novaMensagem = "Nova mensagem";
+        postagem.setMensagem(novaMensagem);
+
+        assertEquals(novaMensagem, postagem.getMensagem());
+    }
+
+    @Test
+     void testAtualizacaoDoTimestamp() {
+        String mensagem = "Mensagem original";
+        Postagem postagem = new Postagem(mensagem);
+        Instant timestampOriginal = postagem.getCreatedAt();
+        // Aguarde um curto período de tempo para simular uma atualização
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+        postagem.setUpdateAt(Instant.now());
+
+        Instant novoTimestamp = postagem.getUpdateAt();
+
+        assertNotEquals(timestampOriginal, novoTimestamp);
+    }
+
+    @Test
+     void testIdNaoNulo() {
+        String mensagem = "Mensagem de teste";
+        Postagem postagem = new Postagem(mensagem);
+        postagem.setId(2L);
+
+        assertNotNull(postagem.getId());
     }
 }
