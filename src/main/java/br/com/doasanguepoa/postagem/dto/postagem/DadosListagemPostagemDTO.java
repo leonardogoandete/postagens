142da177ae2c25a0fcd0cbae7cf36daecac41d5f
leonardo_goandete <leonardo_goandete>
2023-11-07 11:27:42 -0300

    Ajustado DTO

--- src/main/java/br/com/doasanguepoa/postagem/dto/postagem/DadosListagemPostagemDTO.java
+++ src/main/java/br/com/doasanguepoa/postagem/dto/postagem/DadosListagemPostagemDTO.java
@@ -1,12 +1,6 @@
 package br.com.doasanguepoa.postagem.dto.postagem;
 
 import br.com.doasanguepoa.postagem.model.Postagem;
-import lombok.Getter;
-import lombok.Setter;
 
 
-public record DadosListagemPostagemDTO(Long id,String mensagem) {
-        public DadosListagemPostagemDTO(Postagem postagem){
-                this(postagem.getId(), postagem.getMensagem());
-        }
-}
+public record DadosListagemPostagemDTO(Long id,String mensagem) {}
