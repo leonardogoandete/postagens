859d99021c7c2745e314114d627afc20eaeef0ff
Leonardo Goandete <38137707+leonardogoandete@users.noreply.github.com>
2024-05-22 19:59:37 -0300

    Add create table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS postagens (
+    createdAt TIMESTAMP(6) WITH TIME ZONE,
+    id BIGSERIAL NOT NULL,
+    updateAt TIMESTAMP(6) WITH TIME ZONE,
+    cnpj VARCHAR(255),
+    mensagem VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
 insert into postagens (id, mensagem, cnpj, createdAt, updateAt) values(997, 'Sangue AB+', '87020517000120', '2023-10-19 16:55:59.806000 +00:00', '2023-10-19 16:55:59 +00:00');
 insert into postagens (id, mensagem, cnpj, createdAt, updateAt) values(998, 'Sangue O+', '93712735000200', '2023-10-19 16:55:59.806000 +00:00', '2023-10-19 16:55:59 +00:00');
 insert into postagens (id, mensagem, cnpj, createdAt, updateAt) values(999, 'Sangue A-', '87020517000120', '2023-10-19 16:55:59.806000 +00:00', '2023-10-19 16:55:59 +00:00');
